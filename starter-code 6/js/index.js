c0f631da3b7201348b944f834a9f06ce6d3b1825
Tech-A67 <mnash@spartaglobal.com>
2018-04-25 18:01:39 +0100

    updated html classes and js logic to show different methods

--- starter-code 6/js/index.js
+++ starter-code 6/js/index.js
@@ -1,7 +1,11 @@
+// Variables
 var dice = 1;
+var displaySquares = document.getElementsByClassName('square');
+var display5 = document.getElementsByClassName('five');
 
-var displaySquares = document.getElementsByClassName('square')
+// Functions
 
+// Run before every roll to reset the display
 function clearDisplay() {
   for(var i = 0; i < displaySquares.length; i++) {
    displaySquares[i].style.backgroundColor="white";
@@ -11,14 +15,13 @@ function clearDisplay() {
 
 function rollDice() {
   dice = Math.floor(Math.random() * 6) + 1;
-  console.log(dice);
+  console.log(dice); // For verification
   displayResult();
 }
 
 function displayResult() {
   switch(dice) {
     case 1:
-    console.log(displaySquares[5])
       displaySquares[4].style.backgroundColor="black";
       break;
     case 2:
@@ -37,20 +40,14 @@ function displayResult() {
       displaySquares[8].style.backgroundColor="black";
       break;
     case 5:
-      displaySquares[0].style.backgroundColor="black";
-      displaySquares[2].style.backgroundColor="black";
-      displaySquares[4].style.backgroundColor="black";
-      displaySquares[6].style.backgroundColor="black";
-      displaySquares[8].style.backgroundColor="black";
+      for(var i = 0; i < display5.length; i++) {
+       display5[i].style.backgroundColor="black";
+      }
       break;
     case 6:
-      displaySquares[0].style.backgroundColor="black";
-      displaySquares[2].style.backgroundColor="black";
-      displaySquares[3].style.backgroundColor="black";
-      displaySquares[5].style.backgroundColor="black";
-      displaySquares[6].style.backgroundColor="black";
-      displaySquares[8].style.backgroundColor="black";
+      for(var i = 0; i < document.getElementsByClassName('six').length; i++) {
+       document.getElementsByClassName('six')[i].style.backgroundColor="black";
+      }
       break;
   }
-
 }
